690286bb0c734e48a713cb12c6c1dbb54a602cbb
Luciano Moliterno <132705299+LucianoMoliterno@users.noreply.github.com>
2025-04-24 11:47:44 -0300

    Delete AYED directory

--- AYED/Parciales/2022/Recuperatorio2022T1/2_Errores_Codigo/include/Circulo.h
+++
@@ -1,7 +0,0 @@
-#ifndef CIRCULO_H
-#define CIRCULO_H
-
-
-float calcularLongitudArco(Circulo &circulo, float grados);
-
-#endif // CIRCULO_H
--- AYED/TPS Cuatrimestral/TP_Cine/Snack.cpp
+++
@@ -1,4 +0,0 @@
-#include "Snack.h"
-
-// Constructor del snack
-Snack::Snack(string n, double p) : nombre(n), precio(p) {}
